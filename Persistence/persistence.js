897ff11c11df2bd9722ae596d89ebde41920531a
Isai-gomez <isaigomezhernandez@gmail.com>
2023-01-19 21:08:17 -0600

    Retos terminados

--- Persistence/persistence.js
+++ Persistence/persistence.js
@@ -1,14 +1,14 @@
 export const persistence = (num) => {
-  let times = 0;
-  num = num.toString();
+  let times = 0
+  num = num.toString()
   while (num.length > 1) {
-    times++;
-    console.log(num.split("").map(Number));
+    times++
+
     num = num
-      .split("")
+      .split('')
       .map(Number)
       .reduce((a, b) => a * b)
-      .toString();
+      .toString()
   }
-  return times;
-};
+  return times
+}
