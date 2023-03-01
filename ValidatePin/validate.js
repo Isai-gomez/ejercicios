2fc8c4d4b821f40dd1e01f78d4c120dd40c53ef2
Chai <isaigomezhernandez@gmail.com>
2023-03-01 15:21:22 -0600

    simplificar expresion

--- ValidatePin/validate.js
+++ ValidatePin/validate.js
@@ -2,10 +2,9 @@ export function validatePIN (pin) {
     const isnumber = new RegExp(/^[0-9]*$/)
     const result = isnumber.test(pin)
     // evaluate and return true or false
-    if(result){
+    
 
-        return (pin.length===6 || pin.length===4)?true:false;
-    }
-    return false
-    ;
+        return result && (pin.length===6 || pin.length===4);
+   
+    
 }
