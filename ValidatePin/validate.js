0d61f6b05b654adc0a39d93c2516d9d9b93f787a
Chai <isaigomezhernandez@gmail.com>
2023-03-01 16:19:08 -0600

    Codigo formatiado

--- ValidatePin/validate.js
+++ ValidatePin/validate.js
@@ -2,9 +2,5 @@ export function validatePIN (pin) {
     const isnumber = new RegExp(/^[0-9]*$/)
     const result = isnumber.test(pin)
     // evaluate and return true or false
-    
-
-        return result && (pin.length===6 || pin.length===4);
-   
-    
+    return result && (pin.length===6 || pin.length===4);
 }
